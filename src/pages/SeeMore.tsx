9d9b592c8aa2884290282e64d71d648c005d59ef
ksh <kangshqwert@naver.com>
2023-04-03 14:17:19 +0900

    [기능추가]: 더보기 페이지 - 유저 정보, 아이콘 버튼리스트 컴포넌트 추가

--- src/pages/SeeMore.tsx
+++ src/pages/SeeMore.tsx
@@ -1,3 +1,31 @@
+import styled from '@emotion/styled/macro';
+
+import BottomNavigation from '../components/BottomNavigation';
+import IconButtonList from '../components/SeeMore/IconButtonList';
+import UserInfo from '../components/SeeMore/UserInfo';
+import TopNavigation from '../components/TopNavigation';
+
+const Base = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 0 12px;
+  box-sizing: border-box;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 export default function SeeMore() {
-  return <div>SeeMore</div>;
+  return (
+    <Base>
+      <Container>
+        <TopNavigation title="더보기" />
+        <UserInfo username="홍길동" telNo="010-9999-9999" />
+        <IconButtonList />
+        <BottomNavigation />
+      </Container>
+    </Base>
+  );
 }
