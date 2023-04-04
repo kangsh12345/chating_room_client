584e41aca8662e561f297d7217daf128c0f986b6
ksh <kangshqwert@naver.com>
2023-04-04 10:43:43 +0900

    [기능추가]: 채팅방 기능 추가

---
+++ src/components/ChatRoomList/ChatRoom.tsx
@@ -0,0 +1,96 @@
+import { useTheme } from '@emotion/react';
+import styled from '@emotion/styled/macro';
+import { Link } from 'react-router-dom';
+
+const StyledLink = styled(Link)<{ color: string }>`
+  text-decoration: none;
+  color: ${({ color }) => color};
+`;
+
+const Base = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px 0;
+  border-bottom: 1px solid ${({ color }) => color};
+`;
+
+const AvatarWrapper = styled.div`
+  width: 48px;
+  height: 48px;
+`;
+
+const Avatar = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  object-fit: contain;
+`;
+
+const Content = styled.div`
+  flex: 0 1 250px;
+  max-width: 250px;
+  display: flex;
+  flex-direction: column;
+  margin-left: 5px;
+`;
+
+const Username = styled.p`
+  margin: 0;
+  padding: 0;
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+const LastMessage = styled.p<{ color: string }>`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  margin: 8px 0 0 0;
+  padding: 0;
+  font-size: 12px;
+  color: ${({ color }) => color};
+`;
+
+const SentAtWrapper = styled.div`
+  flex: 0 1 36px;
+  text-align: center;
+  font-size: 12px;
+`;
+
+const SentAt = styled.time``;
+
+interface Props {
+  id: number;
+  username: string;
+  lastMessage?: string;
+  lastMessageSentAt?: string;
+}
+
+export default function ChatRoom({
+  id,
+  username,
+  lastMessage,
+  lastMessageSentAt,
+}: Props) {
+  const theme = useTheme();
+
+  return (
+    <StyledLink to={`/rooms/${id}`} color={theme.colors.gray[900]}>
+      <Base color={theme.colors.gray[100]}>
+        <AvatarWrapper>
+          <Avatar src="/placeholder.png" />
+        </AvatarWrapper>
+        <Content>
+          <Username>{username}</Username>
+          <LastMessage color={theme.colors.gray[100]}>
+            {lastMessage}
+          </LastMessage>
+        </Content>
+        <SentAtWrapper>
+          <SentAt>{lastMessageSentAt}</SentAt>
+        </SentAtWrapper>
+      </Base>
+    </StyledLink>
+  );
+}
--- src/pages/RoomList.tsx
+++ src/pages/RoomList.tsx
@@ -1,3 +1,50 @@
+import styled from '@emotion/styled/macro';
+import { AxiosError, AxiosResponse } from 'axios';
+import { useQuery } from 'react-query';
+
+import { fetchChatRoomList } from '../apis/roomApi';
+import BottomNavigation from '../components/BottomNavigation';
+import ChatRoomList from '../components/ChatRoomList';
+import ChatRoom from '../components/ChatRoomList/ChatRoom';
+import TopNavigation from '../components/TopNavigation';
+import { IRoom } from '../types';
+
+const Base = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 0 12px;
+  box-sizing: border-box;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 export default function RoomList() {
-  return <div>RoomList</div>;
+  const { data: chatRoomListData } = useQuery<
+    AxiosResponse<IRoom[]>,
+    AxiosError
+  >('fetchChatRoomList', fetchChatRoomList);
+
+  return (
+    <Base>
+      <Container>
+        <TopNavigation title="채팅" />
+        {chatRoomListData && (
+          <ChatRoomList>
+            {chatRoomListData.data.map(chatRoom => (
+              <ChatRoom
+                key={chatRoom.id}
+                id={chatRoom.id}
+                username={chatRoom.user.username}
+              />
+            ))}
+          </ChatRoomList>
+        )}
+
+        <BottomNavigation />
+      </Container>
+    </Base>
+  );
 }
